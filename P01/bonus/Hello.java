449dfcfa7ce2bd31a3f213f09fb695be66618b79
Angel <axc7208@mavs.uta.edu>
2023-08-27 13:40:48 -0500

    P01 bonus

--- P01/bonus/Hello.java
+++ P01/bonus/Hello.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Hello {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 	// add user input for name
-        System.out.println("Hello, world");
-	}
+	    // scanner ?, 
+	    
+	    Scanner user_name = new Scanner(System.in);
+	    
+	    System.out.println("What is your name?"); 
+	    
+	    String name = user_name.nextLine();
+	    
+        System.out.println("Hello, " + name + "!");
+    }
 }
