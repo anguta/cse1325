f724d149c7db3d28e313a09c7c5573726b1453d4
Angel <axc7208@mavs.uta.edu>
2023-08-27 12:56:12 -0500

    P01 extreme_bonus

--- P01/extreme_bonus/Hello.java
+++ P01/extreme_bonus/Hello.java
@@ -2,14 +2,7 @@ import java.util.Scanner;
 
 public class Hello {
     public static void main(String[] args) {
-	// add user input for name
-	    // scanner ?, 
-	    
-	    // Scanner user_name = new Scanner(System.in);
-	    
-	    // System.out.println("What is your name?"); 
-	    
-	    // String name = user_name.nextLine();
+	    // scanner ?
 	    
 	    String user_name = System.getProperty("user.name");
 	    
