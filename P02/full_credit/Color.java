a44dada375f56fbb07cec091c5af76646ca1d652
Angel <axc7208@mavs.uta.edu>
2023-09-01 21:26:33 -0500

    full credit testing

---
+++ P02/full_credit/Color.java
@@ -0,0 +1,6 @@
+public enum Color {
+	PURPLE,
+	CYAN,
+	PINK,
+	GREEN,
+}
---
+++ P02/full_credit/TestLine.java
@@ -0,0 +1,16 @@
+public class TestLine{
+
+	public static void main(String[] args) {
+		
+		Color color1 = new Color(ORANGE, 2.323, 3.314, 5.515, 9.313); // ???
+		
+		// print 
+		
+        
+        // do i hardcode the numbers ??? hmmm
+        
+        
+		
+		
+	}
+}
