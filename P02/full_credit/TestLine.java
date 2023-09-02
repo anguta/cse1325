0669390727d71b04aaa46d6e2d8f5d632e492840
Angel <axc7208@mavs.uta.edu>
2023-09-01 22:59:43 -0500

    full credit testing

--- P02/full_credit/TestLine.java
+++ P02/full_credit/TestLine.java
@@ -2,12 +2,18 @@ public class TestLine{
 
 	public static void main(String[] args) {
 		
-		Color color1 = new Color(ORANGE, 2.323, 3.314, 5.515, 9.313); // ???
+		Line line1 = new Line(Color.PURPLE, 2.323, 3.314, 5.515, 9.313); // ???
 		
 		// print 
 		
+		// no response from TA or prof = i will just hard code the numbers... maybe
+		
         
         // do i hardcode the numbers ??? hmmm
+        
+        
+        System.out.println(line1.toString() + " has length " + line1.length());
+        
         
         
 		
