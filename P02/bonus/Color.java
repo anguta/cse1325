54ec94f5c9cff6b1077c7179eade3756ba2155e2
Angel <axc7208@mavs.uta.edu>
2023-09-02 22:28:09 -0500

    bonus testing

---
+++ P02/bonus/Color.java
@@ -0,0 +1,19 @@
+public enum Color {
+	PURPLE(0x800080),
+	CYAN(0x00FFFF),
+	PINK(0xFFC0CB),
+	ORANGE(0xFFA500);
+	
+	private final int rgb;
+	
+	private Color(int rgb) {
+	    this.rgb = rgb;
+	}
+	
+	@Override
+	// toString
+	public String toString() {
+	    return this.name() + " (" + String.format("0x%06X",rgb) + ") ";
+	} 
+
+}
