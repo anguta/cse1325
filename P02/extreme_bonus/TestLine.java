17148a86c3eae99ad30c84b61e246defbe328976
Angel <axc7208@mavs.uta.edu>
2023-09-04 15:30:52 -0500

    P02 extreme bonus

--- P02/extreme_bonus/TestLine.java
+++ P02/extreme_bonus/TestLine.java
@@ -7,7 +7,7 @@ public class TestLine{
         Line line3 = new Line(5.419, 8.229, 3.512, 4.056, Color.PINK);
         Line line4 = new Line(9.660, 7.412, 9.765, 1.750, Color.ORANGE);
         
-        System.out.println(/*"\u001B[35m" + */line1.toString() + "\u001B[0m" + " has length " + line1.length());
+        System.out.println(/*"\u001B[35m" + */line1.toString() /*+ "\u001B[0m"*/ + " has length " + line1.length());
         System.out.println(/*"\u001B[36m" + */line2.toString() + /*"\u001B[0m" + */" has length " + line2.length());
         System.out.println(/*"\u001B[38;5;212m" +*/ line3.toString() /*+ "\u001B[0m"*/ + " has length " + line3.length());
         System.out.println(/*"\u001B[38;5;202m" +*/ line4.toString() + /*"\u001B[0m" + */ " has length " + line4.length());
