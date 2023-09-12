de1ad32d3d945b0844b056eccb85982e5c349973
Angel <axc7208@mavs.uta.edu>
2023-09-11 21:35:48 -0500

    P03 full credit

--- P03/full_credit/LibraryManager.java
+++ P03/full_credit/LibraryManager.java
@@ -9,12 +9,12 @@ public class LibraryManager {
         library.addPublication(new Publication("\"Diary of a Wimpy Kid\"", "Jeff Kinney", 2007));
         library.addPublication(new Publication("\"The Giver\"", "Lois Lowry", 1993));
         library.addPublication(new Publication("\"Blue Lock\"", "Muneyuki Kaneshiro", 2018));
-    
+
         System.out.println(library);
         Scanner scanner = new Scanner(System.in);
     	
     	try {
-    	    
+
             System.out.print("Which book would you like?: "); 
             int book = Integer.parseInt(scanner.nextLine().trim()); // trim allowed EX: "2 " to work alongside EX: "2"
         	
