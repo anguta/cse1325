784ff46bae580a264f3e6b8646c93439cf42fe95
Angel <axc7208@mavs.uta.edu>
2023-10-09 22:58:00 -0500

    P06 bonus

--- P06/bonus/library/Library.java
+++ P06/bonus/library/Library.java
@@ -42,7 +42,7 @@ public class Library {
         int publicationSize = Integer.parseInt(br.readLine());
         for(int i = 0; i < publicationSize; i++) {
             
-            if("publication".equals(br.readLine().trim())) {
+            if(br.readLine().trim().equals("publication") {
                 
                 publications.add(new Publication(br));
             } else {
