b0f0f9470be3281913e268706c66a9e3d07cc625
Angel <axc7208@mavs.uta.edu>
2023-11-06 18:52:13 -0600

    bonus

--- P09/bonus/Rectangle.cpp
+++ P09/bonus/Rectangle.cpp
@@ -4,7 +4,7 @@
         : _height{height}, _width{width} {}
                  
     std::string Rectangle::name() {
-        return std::to_string(_height) + "x" +  std::to_string(_width) + " Rectangle " /*+ " with area " + std::to_string(area())*/ + '\n';
+        return std::to_string(_height) + "x" +  std::to_string(_width) + " Rectangle " /*+ " with area " + std::to_string(area())*/ + '\n'; 
     }
         
     double Rectangle::area() {
